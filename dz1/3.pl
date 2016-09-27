5a835739d2368c75f741a13136c6633e6bbd1a1b
perluhin <perluhin@yandex.ru>
2016-09-27 11:17:27 +0300

    all dz

--- dz1/3.pl
+++ dz1/3.pl
@@ -3,11 +3,16 @@ use Data::Dumper;
 open(F1,"text.txt");
 my $list= [];
 $line=<F1>;
+#убираю первую строчку из вывода - Итого 9672
+$line=<F1>;
 while($line){
 	my @local_list = [];
 	
-	@line=split(/;/,$line);	
-	chomp($line[8]);
+	@line=split(/;/,$line);
+	#удаляем \n
+	if($line[8]){	
+		chomp($line[8]);
+	}
 	push $local_list[0],@line;	
 	push($list,@local_list);
 	
